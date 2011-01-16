b2e371b162994e78e640deb4088d0aacea2d120b
Tim Butram <atrus6@gmail.com>
2011-01-16 17:01:43 -0500

    Added questions to make the gasExample more customizable.

--- src/game/GasExample.java
+++ src/game/GasExample.java
@@ -7,6 +7,8 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 import level.Room;
 
 public class GasExample implements KeyListener
@@ -15,16 +17,16 @@ public class GasExample implements KeyListener
 	private Random ran = new Random();
 
 	
-	public GasExample()
+	public GasExample(boolean pixelPerfect, int number)
 	{
 		room = new Room(800, 800, 1);
 		
-		for(int i = 0; i < 10; i++)
+		for(int i = 0; i < number; i++)
 		{
 			room.addSprite(new Particle(ran.nextInt(room.getWidth()), ran.nextInt(room.getHeight()), room));
 		}
 		
-		Game g = new Game(room, false, new Dimension(800, 800), "Gas Test");
+		Game g = new Game(room, pixelPerfect, new Dimension(800, 800), "Gas Test");
 		g.addKeyListener(this);
 	}
 
@@ -39,7 +41,26 @@ public class GasExample implements KeyListener
 	
 	public static void main(String args[])
 	{
-		new GasExample();
+		int option = JOptionPane.showConfirmDialog(null, "Do you want Pixel Perfect?", "Pixel Perfect?", JOptionPane.YES_NO_OPTION);
+		boolean pp = false;
+		
+		if(option == JOptionPane.YES_OPTION)
+		{
+			pp = true;
+		}
+		
+		String s = JOptionPane.showInputDialog(null, "Number of Particles?");
+		int number = 10;
+		try
+		{
+			number = Integer.parseInt(s);
+		}
+		catch(Exception e)
+		{
+			number = 10;
+		}
+		
+		new GasExample(pp, number);
 	}
 
 	@Override
